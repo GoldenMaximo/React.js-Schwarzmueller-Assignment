22031ed2f95b6a94ea7ac26e0d7405249fb95263
Gustavo Máximo <gfmaximo97@gmail.com>
2019-11-12 15:35:57 -0300

    Updates

---
+++ precache-manifest.fc1a0fcdcff07855ffe52e8891f4b87b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f69da21123737f370aff24c2e5e2e32",
+    "url": "/React.js-Schwarzmueller-Assignment/index.html"
+  },
+  {
+    "revision": "b61dda52ec6d4cfda13d",
+    "url": "/React.js-Schwarzmueller-Assignment/static/css/main.f84f4ad2.chunk.css"
+  },
+  {
+    "revision": "cfc9fa7b7506cb82a8b3",
+    "url": "/React.js-Schwarzmueller-Assignment/static/js/2.3a67db91.chunk.js"
+  },
+  {
+    "revision": "b61dda52ec6d4cfda13d",
+    "url": "/React.js-Schwarzmueller-Assignment/static/js/main.d52c108f.chunk.js"
+  },
+  {
+    "revision": "d5a9ac5a85d9ccbf9f4b",
+    "url": "/React.js-Schwarzmueller-Assignment/static/js/runtime-main.06f7c87e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.84e85fae33063e270a2c92212b497d3a.js"
+  "/React.js-Schwarzmueller-Assignment/precache-manifest.fc1a0fcdcff07855ffe52e8891f4b87b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React.js-Schwarzmueller-Assignment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
